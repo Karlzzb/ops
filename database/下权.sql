a80917567589516e0b70dff63704167938d6ec7c
Karlzzb <lansedepiaomiao@gmail.com>
2016-04-28 12:29:58 +0700

    modify

--- database/下权.sql
+++ database/下权.sql
@@ -16,3 +16,17 @@
 
  
  select distinct uid, username from (select * from s8_bbs.tmp_expired_groups41 union all select * from s8_bbs.tmp_expired_groups42) as a;
+ 
+ 
+ drop table if exists s8_bbs.tmp_expired_groups_spical;
+ create table s8_bbs.tmp_expired_groups_spical select m.uid, m.username, m.groupid, m.groups from s8_bbs.pw_members m join s8_bbs.pw_extragroups g on m.uid = g.uid where g.gid in (120,121,122,123,124,125) and g.startdate < unix_timestamp(date_add(now(),interval - g.days day));
+ alter table s8_bbs.tmp_expired_groups_spical add key uid_key(uid);
+ start transaction;
+ update s8_bbs.tmp_expired_groups_spical g, s8_bbs.pw_members m set m.groupid = -1 where m.uid = g.uid and m.groupid in (120,121,122,123,124,125) and g.groupid in (120,121,122,123,124,125);
+ update s8_bbs.tmp_expired_groups_spical g, s8_bbs.pw_members m set m.groups = replace(m.groups,'120','') where m.uid = g.uid;
+ update s8_bbs.tmp_expired_groups_spical g, s8_bbs.pw_members m set m.groups = replace(m.groups,'121','') where m.uid = g.uid;
+ update s8_bbs.tmp_expired_groups_spical g, s8_bbs.pw_members m set m.groups = replace(m.groups,'122','') where m.uid = g.uid;
+ update s8_bbs.tmp_expired_groups_spical g, s8_bbs.pw_members m set m.groups = replace(m.groups,'123','') where m.uid = g.uid;
+ update s8_bbs.tmp_expired_groups_spical g, s8_bbs.pw_members m set m.groups = replace(m.groups,'124','') where m.uid = g.uid;
+ update s8_bbs.tmp_expired_groups_spical g, s8_bbs.pw_members m set m.groups = replace(m.groups,'125','') where m.uid = g.uid;
+ delete from s8_bbs.pw_extragroups  where gid in (120,121,122,123,124,125) and startdate < unix_timestamp(date_add(now(),interval - days day));
