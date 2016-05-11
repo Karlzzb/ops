2e1721722d16521fff9980ecadb0c3d4bae47b21
Karlzzb <lansedepiaomiao@gmail.com>
2016-05-11 08:58:16 +0700

    modify

---
+++ database/mysql-test/tpcc/tpcc-graph-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+### goto user homedir and remove previous file
+rm -f '$2'
+gnuplot << EOP
+### set data source file
+datafile = '$1'
+### set graph type and size
+set terminal jpeg size 640,480
+### set titles
+set grid x y
+set xlabel "Time (sec)"
+set ylabel "Transactions"
+### set output filename
+set output '$2'
+### build graph
+# plot datafile with lines
+plot datafile using 1:2 title "Mysql Performance" ls 1 with lines,datafile using 3:4 title "Mysql Performance skip slow log" ls 2 with lines axes x1y1
+EOP
---
+++ database/mysql-test/tpcc/tpcc-output-analyze.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+TIMESLOT=1
+if [ -n "$2" ]
+then
+TIMESLOT=$2
+fi
+cat $1 | grep -v HY000 | grep -v payment | grep -v neword | awk -v timeslot=$TIMESLOT 'BEGIN { FS="[,():]"; s=0; cntr=0; aggr=0 } /MEASURING START/ { s=1} /STOPPING THREADS/ {s=0} /0/ { if (s==1) { cntr++; aggr+=$2; } if ( cntr==timeslot ) { printf ("%d %3d\n",$1,(aggr/timeslot)) ; cntr=0; aggr=0 } }'
